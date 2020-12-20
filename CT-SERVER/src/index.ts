f7f5d0d8ff5233319a9b58ee16ed42be95a2ce19
psyCOSMO <max.funoff@gmail.com>
2020-12-20 21:06:33 +0200

    Init graphql and resolvers

---
+++ CT-SERVER/src/index.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config'
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './reslovers/post';
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+
+    const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('✅ Server stated on http://localhost:4000/')
+    })
+}
+
+main().catch((err) => {
+    console.log(err)
+});
